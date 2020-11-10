0ab228fdfb31eeaa3db48508a10e6209fa980eca
galileu.kim <galileukim@gmail.com>
2020-11-10 15:57:57 -0500

    output descriptives for school principal saeb

--- source/descriptive/modules/visual_principal.R
+++ source/descriptive/modules/visual_principal.R
@@ -8,21 +8,21 @@ saeb_principal <- read_data(
 )
 
 # ---------------------------------------------------------------------------- #
-plot_histogram <- function(data, var){
-    data %>%
-    ggplot(
-        aes(
-            x = {{var}},
-            group = year,
-            fill = as.factor(year)
-        )
-    ) +
-    stat_count(
-        width = 0.5,
-        position = "dodge"
-    ) +
-    theme(legend.title = element_blank())
-}
+plot_histogram <- function(data, var) {
+    plot <- data %>%
+        ggplot(
+            aes(
+                x = {{ var }},
+                group = year,
+                fill = as.factor(year)
+            )
+        ) +
+        stat_count(
+            width = 0.5,
+            position = "dodge"
+        ) +
+        labs(x = "", y = "", fill = "Year")
+        }
 
 # ---------------------------------------------------------------------------- #
 saeb_principal <- saeb_principal %>%
@@ -31,20 +31,38 @@ saeb_principal <- saeb_principal %>%
 plot_experience <- saeb_principal %>%
     plot_histogram(
         saeb_principal_experience
-    )
+    ) +
+    ggtitle("Years of Experience")
 
 plot_appointment <- saeb_principal %>%
     filter(!is.na(saeb_principal_appointment)) %>%
     plot_histogram(
         saeb_principal_appointment
-    )
+    ) +
+    ggtitle("Appointment Process")
 
 plot_education <- saeb_principal %>%
     plot_histogram(
         saeb_principal_education
-    )
+    ) +
+    ggtitle("Education")
 
 plot_age <- saeb_principal %>%
     plot_histogram(
         saeb_principal_age
-    )
+    ) +
+    ggtitle("Age")
+
+plot_principal_descriptives <- ggpubr::ggarrange(    
+        plot_experience,
+        plot_appointment,
+        plot_education,
+        plot_age,
+        common.legend = TRUE,
+        legend = "bottom"
+)
+
+save_fig(
+    plot_principal_descriptives,
+    "plot_principal_descriptive.pdf"
+)
