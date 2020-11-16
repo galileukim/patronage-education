2f52e43697c6d501d30e43e3a4fc63f80524ef42
galileu.kim <galileukim@gmail.com>
2020-11-16 18:28:44 -0500

    create joint descriptives

---
+++ source/descriptive/modules/visual_descriptives.R
@@ -0,0 +1,27 @@
+# produce descriptive statistics on both teachers and school principals
+# start with simple visualizations
+rais_edu_year <- read_data(
+    "rais",
+    "rais_year_edu.rds"
+)
+
+# ---------------------------------------------------------------------------- #
+# aux funs
+ggplot_rais_by_year <- function(data, var){
+    ggplot(
+        data,
+        aes(year, {{var}}, color = rais_category)
+    ) +
+    geom_point(size = 3) +
+    scale_x_continuous(
+        breaks = seq(2005, 2013, 4),
+        labels = seq(2005, 2013, 4)
+    )
+}
+
+plot_hired <- map(
+         c(rais_hired, rais_higher_edu, rais_wage, rais_permanent),
+        ~ ggplot_rais_by_year(rais_edu_year, .)
+    )
+
+plot_edu <- rais_edu_year
